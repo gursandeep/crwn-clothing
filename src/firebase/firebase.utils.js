e8a1aa2d3edb6e25c34f98e4810ab1e5f01c41d2
Gursandeep Kainth <Gursandeep@zenithpayments.com.au>
2020-04-12 22:17:37 +1000

    Committing

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,15 +12,38 @@ const config = {
   appId: "1:973212457805:web:ced611ea5f1b6e8fa7f504",
   measurementId: "G-7RQYY09MPV"
 };
-
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
-export const fireStore = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
