e2c2995b66fc07a910506a800bd0598863c6b9dc
Gursandeep Kainth <Gursandeep@zenithpayments.com.au>
2020-04-29 23:43:01 +1000

    updated shop data with new data, updated cart util to group

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
-import rootReducer from "./root-reducer";
-const middleWares = [logger];
+import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
